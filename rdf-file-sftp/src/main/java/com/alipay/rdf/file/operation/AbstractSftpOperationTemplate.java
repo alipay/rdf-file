51f35b4a6943945d48c68d2725aa38e44b068aca
haofan.whf <haofan.whf@alibaba-inc.com>
2018-10-10 21:32:00 +0800

    Sftphelp fix

--- rdf-file-sftp/src/main/java/com/alipay/rdf/file/operation/AbstractSftpOperationTemplate.java
+++ rdf-file-sftp/src/main/java/com/alipay/rdf/file/operation/AbstractSftpOperationTemplate.java
@@ -8,14 +8,10 @@ import java.util.Map;
 
 import com.alipay.rdf.file.exception.RdfErrorEnum;
 import com.alipay.rdf.file.exception.RdfFileException;
-import com.alipay.rdf.file.interfaces.FileSftpStorageConstants;
-import com.alipay.rdf.file.util.JschFactory;
 import com.alipay.rdf.file.util.RdfFileLogUtil;
+import com.alipay.rdf.file.util.SFTPHelper;
 import com.alipay.rdf.file.util.SFTPUserInfo;
-import com.alipay.rdf.file.util.SftpThreadContext;
 import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
 
 /**
  *
@@ -64,7 +60,7 @@ public abstract class AbstractSftpOperationTemplate<T> {
                 throw new RdfFileException("rdf-file#sftpOperation." + this.operationType
                         + ".checkParams fail,params=" + params, RdfErrorEnum.ILLEGAL_ARGUMENT);
             }
-            sftp = openChannelSftp(user);
+            sftp = SFTPHelper.openChannelSftp(user);
             response = doBusiness(user, params);
         } catch (Exception e){
             RdfFileLogUtil.common.warn("rdf-file#sftpOperation."
@@ -72,63 +68,11 @@ public abstract class AbstractSftpOperationTemplate<T> {
             response.setSuccess(false);
             response.setError(e);
         } finally {
-            closeConnection(sftp, user);
+            SFTPHelper.closeConnection(sftp, user);
         }
         RdfFileLogUtil.common.info("rdf-file#sftpOperation."
                 + this.operationType + ".response,result=" + response + ",params=" + params);
         return response;
-    }
-
-    /**
-     * 关闭连接
-     *
-     * @param sftp
-     * @param user
-     */
-    private static void closeConnection(ChannelSftp sftp, SFTPUserInfo user) {
-        try {
-            sftp.disconnect();
-            if(sftp.getSession() != null){
-                sftp.getSession().disconnect();
-            }
-        } catch (Exception e) {
-            RdfFileLogUtil.common.warn("rdf-file#closeConnection fail"
-                    + ",user={" + user.toString(true, true) + "}", e);
-        }finally {
-            SftpThreadContext.clearChannelSftp();
-        }
-    }
-
-    /**
-     * 根据SFTP配置打开SFTP通道
-     * @param user
-     * @return
-     * @throws JSchException
-     */
-    private static ChannelSftp openChannelSftp(SFTPUserInfo user) throws JSchException {
-
-        RdfFileLogUtil.common.debug("rdf-file#SFTPHelper.openChannelSftp request"
-                + ",user={" + user.toString(true, false) + "}");
-        // 创建ssh会话
-        Session ssh = JschFactory.openConnection(user);
-
-        RdfFileLogUtil.common.debug("rdf-file#SFTPHelper.openChannelSftp create ssh success"
-                + ",user={" + user.toString(true, false) + "}");
-
-        // 打开sftp连接
-        ChannelSftp channel = (ChannelSftp) ssh.openChannel(FileSftpStorageConstants.SFTP);
-
-        if(channel == null){
-            throw new RdfFileException("rdf-file#SFTPHelper.openChannelSftp get ChannelSftp fail.", RdfErrorEnum.UNKOWN);
-        }
-
-        SftpThreadContext.setChannelSftp(channel);
-
-        channel.connect();
-
-        RdfFileLogUtil.common.debug("rdf-file#SFTPHelper.openChannelSftp create channel success"
-                + ",user={" + user.toString(true, false) + "}");
-        return channel;
     }
 
     /**
