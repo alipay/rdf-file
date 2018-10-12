da2e3a9101de13a16bb457a51f1a90e4171c8911
hongwei.quhw <hongwei.quhw@antfin.com>
2018-10-12 11:04:39 +0800

    body多模板支持

--- rdf-file-core/src/main/java/com/alipay/rdf/file/loader/TemplateLoader.java
+++ rdf-file-core/src/main/java/com/alipay/rdf/file/loader/TemplateLoader.java
@@ -3,8 +3,6 @@ package com.alipay.rdf.file.loader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
