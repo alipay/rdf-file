5413e91489e7e03ca24f43e7cd665542938b872b
hongwei.quhw <hongwei.quhw@antfin.com>
2018-10-14 20:14:38 +0800

    回调匹配规则实现

--- rdf-file-core/src/main/java/com/alipay/rdf/file/condition/CallbackRowCondition.java
+++ rdf-file-core/src/main/java/com/alipay/rdf/file/condition/CallbackRowCondition.java
@@ -4,6 +4,7 @@ import com.alipay.rdf.file.meta.FileBodyMeta;
 import com.alipay.rdf.file.model.FileConfig;
 import com.alipay.rdf.file.spi.RdfFileRowConditionSpi;
 import com.alipay.rdf.file.util.BeanMapWrapper;
+import com.alipay.rdf.file.util.RdfFileUtil;
 
 /**
  * Copyright (C) 2013-2018 Ant Financial Services Group
@@ -14,19 +15,22 @@ import com.alipay.rdf.file.util.BeanMapWrapper;
  * @version $Id: CallbackRowCondition.java, v 0.1 2018年10月11日 下午8:49:34 hongwei.quhw Exp $
  */
 public class CallbackRowCondition implements RdfFileRowConditionSpi {
+    private RdfFileRowConditionSpi callbackRowCondition;
 
     @Override
     public void init(FileBodyMeta bodyMeta) {
+        this.callbackRowCondition = (RdfFileRowConditionSpi) RdfFileUtil
+            .newInstance(bodyMeta.getRowConditionParam());
     }
 
     @Override
     public boolean serialize(FileConfig config, BeanMapWrapper row) {
-        return false;
+        return callbackRowCondition.serialize(config, row);
     }
 
     @Override
     public boolean deserialize(FileConfig fileConfig, String[] row) {
-        return false;
+        return callbackRowCondition.deserialize(fileConfig, row);
     }
 
 }
