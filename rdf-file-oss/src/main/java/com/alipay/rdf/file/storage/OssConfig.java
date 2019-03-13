4d58b5d5c73d1ebf4a6b75b5a25f68f993d824ba
hongwei.quhw <hongwei.quhw@antfin.com>
2019-03-13 19:57:18 +0800

    ossconfig equals hashcode覆写

--- rdf-file-oss/src/main/java/com/alipay/rdf/file/storage/OssConfig.java
+++ rdf-file-oss/src/main/java/com/alipay/rdf/file/storage/OssConfig.java
@@ -13,23 +13,23 @@ import com.aliyun.oss.ClientConfiguration;
  * @version $Id: OssConfig.java, v 0.1 2017年8月24日 上午11:38:34 hongwei.quhw Exp $
  */
 public class OssConfig {
-    public static final String OSS_STORAGE_CONFIG_KEY            = "oss_storage_config";
-    public static final String DEFAUT_OSS_TEMP_ROOT_KEY          = "defaut_oss_temp_root";
-    public static final String DEFAULT_OSS_BIG_FILE_SIZE_KEY     = "default_oss_big_file_size";
-    public static final String DEFAULT_OSS_APPEND_SIZE_LIMIT_KEY = "default_oss_append_size_limit_size";
+    public static final String        OSS_STORAGE_CONFIG_KEY            = "oss_storage_config";
+    public static final String        DEFAUT_OSS_TEMP_ROOT_KEY          = "defaut_oss_temp_root";
+    public static final String        DEFAULT_OSS_BIG_FILE_SIZE_KEY     = "default_oss_big_file_size";
+    public static final String        DEFAULT_OSS_APPEND_SIZE_LIMIT_KEY = "default_oss_append_size_limit_size";
 
     /** 
      * 写文件时OSS本地文件根目录
      * <li>写文件时会先写在本地再上传到OSS
      * <li>OSS本地目录为：tempRoot + OSS路径
      */
-    private static String      DEFAUT_OSS_TEMP_ROOT              = "/home/admin/logs/ossLocal/";
+    private static String             DEFAUT_OSS_TEMP_ROOT              = "/home/admin/logs/ossLocal/";
 
     /** 使用大文件上传接口的文件大小 1G*/
-    private static final long  DEFAULT_BIG_FILE_SIZE             = 1024L * 1024L * 1024L;
+    private static final long         DEFAULT_BIG_FILE_SIZE             = 1024L * 1024L * 1024L;
 
     /**追加上传的次数没有限制，文件大小上限为5GB*/
-    private static final long  OSS_APPEND_SIZE_LIMIT             = 5 * 1024L * 1024L * 1024L;
+    private static final long         OSS_APPEND_SIZE_LIMIT             = 5 * 1024L * 1024L * 1024L;
 
     private final String              bucketName;
     private final String              endpoint;
@@ -41,19 +41,19 @@ public class OssConfig {
      * <li>写文件时会先写在本地再上传到OSS
      * <li>OSS本地目录为：tempRoot + OSS路径
      */
-    private       String              ossTempRoot;
+    private String                    ossTempRoot;
 
-    private Long               ossBigFileSize;
+    private Long                      ossBigFileSize;
 
-    private Long               ossAppendSizeLimit;
+    private Long                      ossAppendSizeLimit;
 
     public OssConfig(String bucketName, String endpoint, String accessKeyId,
                      String accessKeySecret) {
         this(bucketName, endpoint, accessKeyId, accessKeySecret, null);
     }
 
-    public OssConfig(String bucketName, String endpoint, String accessKeyId,
-                     String accessKeySecret, ClientConfiguration clientConfiguration) {
+    public OssConfig(String bucketName, String endpoint, String accessKeyId, String accessKeySecret,
+                     ClientConfiguration clientConfiguration) {
         this.bucketName = bucketName;
         this.endpoint = endpoint;
         this.accessKeyId = accessKeyId;
@@ -131,7 +131,7 @@ public class OssConfig {
         this.ossAppendSizeLimit = ossAppendSizeLimit;
     }
 
-    public ClientConfiguration getClientConfiguration(){
+    public ClientConfiguration getClientConfiguration() {
         return this.clientConfiguration;
     }
 
@@ -143,11 +143,6 @@ public class OssConfig {
         result = prime * result + ((accessKeySecret == null) ? 0 : accessKeySecret.hashCode());
         result = prime * result + ((bucketName == null) ? 0 : bucketName.hashCode());
         result = prime * result + ((endpoint == null) ? 0 : endpoint.hashCode());
-        result = prime * result
-                 + ((ossAppendSizeLimit == null) ? 0 : ossAppendSizeLimit.hashCode());
-        result = prime * result + ((ossBigFileSize == null) ? 0 : ossBigFileSize.hashCode());
-        result = prime * result + ((ossTempRoot == null) ? 0 : ossTempRoot.hashCode());
-        result = prime * result + ((clientConfiguration == null) ? 0 : clientConfiguration.hashCode());
         return result;
     }
 
@@ -179,26 +174,6 @@ public class OssConfig {
             if (other.endpoint != null)
                 return false;
         } else if (!endpoint.equals(other.endpoint))
-            return false;
-        if (ossAppendSizeLimit == null) {
-            if (other.ossAppendSizeLimit != null)
-                return false;
-        } else if (!ossAppendSizeLimit.equals(other.ossAppendSizeLimit))
-            return false;
-        if (ossBigFileSize == null) {
-            if (other.ossBigFileSize != null)
-                return false;
-        } else if (!ossBigFileSize.equals(other.ossBigFileSize))
-            return false;
-        if (ossTempRoot == null) {
-            if (other.ossTempRoot != null)
-                return false;
-        } else if (!ossTempRoot.equals(other.ossTempRoot))
-            return false;
-        if (clientConfiguration == null) {
-            if (other.clientConfiguration != null)
-                return false;
-        } else if (!clientConfiguration.equals(other.clientConfiguration))
             return false;
         return true;
     }
