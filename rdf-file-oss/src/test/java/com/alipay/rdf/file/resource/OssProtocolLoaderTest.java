d8ef9212df818bc495ea59b4d5b5f3927e8e93b0
hongwei.quhw <hongwei.quhw@antfin.com>
2020-07-16 15:52:12 +0800

    modify tests

--- rdf-file-oss/src/test/java/com/alipay/rdf/file/resource/OssProtocolLoaderTest.java
+++ rdf-file-oss/src/test/java/com/alipay/rdf/file/resource/OssProtocolLoaderTest.java
@@ -57,7 +57,7 @@ public class OssProtocolLoaderTest {
 
     @After
     public void after() {
-        fileStorage.delete(OSS_PATH);
+        //fileStorage.delete(OSS_PATH);
         FileDefaultConfig.RDF_PROCESSOR_PATH = "classpath*:META-INF/rdf-file/auto-processor/";
         FileDefaultConfig.RDF_PROTOCOL_PATH = "classpath*:META-INF/rdf-file/protocol/";
         FileDefaultConfig.RDF_FORMAT_PATH = "classpath*:META-INF/rdf-file/format/";
