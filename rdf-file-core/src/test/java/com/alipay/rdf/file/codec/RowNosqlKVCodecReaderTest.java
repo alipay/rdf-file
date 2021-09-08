0b330fc2c15ee1c1829c97b55ff0b4eb1e0e976c
hongwei.quhw <hongwei.quhw@antfin.com>
2021-09-08 15:45:08 +0800

    kvcodectestcase

--- rdf-file-core/src/test/java/com/alipay/rdf/file/codec/RowNosqlKVCodecReaderTest.java
+++ rdf-file-core/src/test/java/com/alipay/rdf/file/codec/RowNosqlKVCodecReaderTest.java
@@ -267,6 +267,51 @@ public class RowNosqlKVCodecReaderTest {
         Assert.assertNull(fileReader.readRow(HashMap.class));
     }
 
+    @Test
+    public void test8() throws Exception {
+        String filePath = File.class.getResource("/codec/kv/data/data6.txt").getPath();
+
+        FileConfig config = new FileConfig(filePath, "/codec/kv/template/template4.json", new StorageConfig("nas"));
+
+        FileReader fileReader = FileFactory.createReader(config);
+
+        Map<String, Object> head = fileReader.readHead(HashMap.class);
+        Assert.assertEquals(100L, head.get("totalCount"));
+        Assert.assertEquals(new BigDecimal("300.03"), head.get("totalAmount"));
+
+        Map<String, Object> row = fileReader.readRow(HashMap.class);
+        Assert.assertEquals("seq_0", row.get("seq"));
+        Assert.assertEquals("inst_seq_0", row.get("instSeq"));
+        Assert.assertEquals("2013-11-09 12:34:56",
+                DateUtil.format((Date) row.get("gmtApply"), "yyyy-MM-dd HH:mm:ss"));
+        Assert.assertEquals("20131109", DateUtil.format((Date) row.get("date"), "yyyyMMdd"));
+        Assert.assertEquals("20131112 12:23:34",
+                DateUtil.format((Date) row.get("dateTime"), "yyyyMMdd HH:mm:ss"));
+        Assert.assertEquals(new BigDecimal("33"), row.get("applyNumber"));
+        Assert.assertEquals(new BigDecimal("10.22"), row.get("amount"));
+        Assert.assertEquals(new Integer(22), row.get("age"));
+        Assert.assertEquals(new Long(12345), row.get("longN"));
+        Assert.assertEquals(Boolean.TRUE, row.get("bol"));
+        Assert.assertEquals("备注1", row.get("memo"));
+
+        row = fileReader.readRow(HashMap.class);
+        Assert.assertEquals("seq_1", row.get("seq"));
+        Assert.assertEquals("inst_seq_1", row.get("instSeq"));
+        Assert.assertEquals("2013-11-10 15:56:12",
+                DateUtil.format((Date) row.get("gmtApply"), "yyyy-MM-dd HH:mm:ss"));
+        Assert.assertEquals("20131110", DateUtil.format((Date) row.get("date"), "yyyyMMdd"));
+        Assert.assertEquals("20131113 12:33:34",
+                DateUtil.format((Date) row.get("dateTime"), "yyyyMMdd HH:mm:ss"));
+        Assert.assertEquals(new BigDecimal("34"), row.get("applyNumber"));
+        Assert.assertEquals(new BigDecimal("11.88"), row.get("amount"));
+        Assert.assertEquals(new Integer(33), row.get("age"));
+        Assert.assertEquals(new Long(56789), row.get("longN"));
+        Assert.assertEquals(Boolean.FALSE, row.get("bol"));
+        Assert.assertEquals(null, row.get("memo"));
+
+        Assert.assertNull(fileReader.readRow(HashMap.class));
+    }
+
     @After
     public void after() {
         tf.delete();
